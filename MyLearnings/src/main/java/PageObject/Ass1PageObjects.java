f9fb4c9d5a76df61a80f74e5ca1d5a0284a7f8a5
Nikita Aiwale <Nikita Aiwale@192.168.2.7>
2022-03-15 13:18:42 +0530

    first commit

---
+++ MyLearnings/src/main/java/PageObject/Ass1PageObjects.java
@@ -0,0 +1,86 @@
+package PageObject;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+
+
+public class Ass1PageObjects{
+	public static WebDriver driver;
+
+public static final By mobile = By.xpath("//a[text()='Mobile']");
+public static final By select = By.xpath("(//select[@title='Sort By'])[1]");
+public static final By Selectname = By.xpath("(//select[@title='Sort By']/option[normalize-space(text())='Name'])[1]");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+public void clickOnMobile() throws Exception {
+	driver.findElement(mobile).click();
+	
+	String actual= driver.getTitle();
+	String expected="Mobile" ;
+	if(actual.equalsIgnoreCase(expected)) {
+		System.out.println(driver.getTitle()+" Title is displayed");
+		
+	}else {
+		System.out.println("Correct Title is not displayed");
+
+	}
+
+	driver.findElement(select).click();
+	Thread.sleep(2000);
+	driver.findElement(Selectname).click();
+	
+	
+}
+
+public void sortByName() {
+    ArrayList <String> notSorted = new ArrayList<String>();
+
+	List<WebElement> allElements = driver.findElements(By.xpath("//h2//a"));
+	
+	for(int i=0;i<=allElements.size()-1;i++) {
+		System.out.println(allElements.size());
+		notSorted.add(allElements.get(i).getText());
+
+	System.out.println("All Elements are " +notSorted);
+	}
+    ArrayList<String> sorted = new ArrayList<String>();
+     for(String sortedList : notSorted) {
+    	 sorted.add(sortedList);
+    	
+     }
+      Collections.sort(sorted);
+		System.out.println("Sorted list is displayed"+sorted);
+
+}
+
+
+
+
+
+
+
+}

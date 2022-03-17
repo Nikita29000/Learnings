8dea0c13029c69061afae1466189ca081b20f6db
Nikita Aiwale <Nikita Aiwale@192.168.2.7>
2022-03-17 17:20:55 +0530

    added utility file

---
+++ com.MyLearnings/src/main/java/com/utility/Screenshots.java
@@ -0,0 +1,23 @@
+package com.utility;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.apache.commons.io.FileUtils;
+
+import com.assigment.pageObjects.Ass1PageObjects;
+
+public class Screenshots extends Ass1PageObjects{
+	
+	public void TakesScreenshots() throws IOException {
+		
+	
+	TakesScreenshot ts = (TakesScreenshot) driver;
+	File source = ts.getScreenshotAs(OutputType.FILE);
+	File destFile=new File("./src/main/resources/Screenshot.png");
+	FileUtils.copyFile(source, destFile);
+	}
+
+}
--- com.MyLearnings/src/test/java/com/assigment/tests/Ass1Test.java
+++ com.MyLearnings/src/test/java/com/assigment/tests/Ass1Test.java
@@ -5,58 +5,52 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import com.assigment.pageObjects.Ass1PageObjects;
+import com.utility.Screenshots;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Ass1Test extends Ass1PageObjects {
 	Ass1PageObjects obj = new Ass1PageObjects();
+	Screenshots screenshot = new Screenshots();
 
-
-	
 	@BeforeMethod
 	public void setProperty() {
-	//	WebDriverManager.chromedriver().setup();
+		driver.manage().deleteAllCookies();
+
 	}
-	
 
 	@BeforeTest
 	public void launchUrl() {
-		System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\Driver\\chromedriver.exe"); 
+		WebDriverManager.chromedriver().setup();
+
+//		System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\Driver\\chromedriver.exe"); 
 		driver = new ChromeDriver();
 		driver.get("http://live.techpanda.org/index.php/");
 		driver.manage().window().maximize();
 
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 	}
-	
-	
-	@Test
-	public void Guru99() throws Exception {
-		
-		String actual= driver.getTitle();
-		String expected="Home page" ;
-		if(expected.equalsIgnoreCase(actual)) {
-			System.out.println(driver.getTitle()+" Title is displayed");
-			
-		}else {
-			System.out.println("Correct Title is not displayed");
 
-		}
-		
+	@Test
+	public void SelectName() throws Exception {
+
+		obj.verifyPagetitle();
 		obj.clickOnMobile();
+//		screenshot.TakesScreenshots();
 		obj.sortByName();
 	}
-	
-	
+
 	@AfterTest
 	public void closeBrowser() {
-		driver.manage().deleteAllCookies();
 		driver.quit();
+
 	}
+
 }
